7a34dac7cfcad1c3faf89792ce57cc740190d944
OwnerOfJK <john.kaller@hotmail.de>
2025-02-20 08:26:44 +0100

    updated submodule

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom"
 import {DojoEngine} from "@pixelaw/core-dojo"
 import {MudEngine} from "@pixelaw/core-mud"
 import {DEFAULT_WORLD, getCoreDefaultsFromUrl} from "@/utils.ts";
-import { StarknetChainProvider } from "@pixelaw/react-dojo"
+// import { StarknetChainProvider } from "@pixelaw/react-dojo"
 
 // TODO for now hardcoded, but planning to retrieve from github URL using env WORLDS_REGISTRY_URL
 import worldsRegistry from "@/config/worlds.json"
@@ -19,8 +19,7 @@ const engines = [DojoEngine, MudEngine]
 
 const coreDefaults = getCoreDefaultsFromUrl()
 
-// TODO this needs improvement, PixelawProvider chooses Engine, but we still need ChainProvider chosen here based on the active engine..
-const ChainProvider = StarknetChainProvider
+// const ChainProvider = StarknetChainProvider
 
 console.log("root")
 if (rootElement) {
@@ -28,9 +27,9 @@ if (rootElement) {
         <React.StrictMode>
             <PixelawProvider worldsRegistry={worldsRegistry} world={DEFAULT_WORLD} engines={engines} coreDefaults={coreDefaults}>
                     <BrowserRouter>
-                        <ChainProvider>
+                        {/* <ChainProvider> */}
                         <Main />
-                        </ChainProvider>
+                        {/* </ChainProvider> */}
                     </BrowserRouter>
                 </PixelawProvider>
         </React.StrictMode>
